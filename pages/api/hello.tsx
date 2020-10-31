e9ea948d106b5845d41c5a3aebb4e30e601a7d4a
antoinechalifour <antoine.chalifour@gmail.com>
2020-10-31 15:34:41 +0100

    configure typescript

---
+++ pages/api/hello.tsx
@@ -0,0 +1,8 @@
+import {NextApiHandler} from "next";
+
+const helloHandler: NextApiHandler = (req, res) => {
+  res.statusCode = 200
+  res.json({ name: 'John Doe' })
+}
+
+export default helloHandler
